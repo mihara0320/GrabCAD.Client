ab59dc17068827aa1319db0188da65b5c951aae6
Masaki <masaki0320@icloud.com>
2019-02-26 01:33:05 +0200

    menu container

---
+++ src/app/core/containers/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { HubService } from '@shared/services/hub.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  playerCount: number;
+
+  constructor(
+    public hub: HubService
+  ) { }
+
+  ngOnInit() {
+    this.hub.players$.subscribe(n => this.playerCount);
+  }
+}
