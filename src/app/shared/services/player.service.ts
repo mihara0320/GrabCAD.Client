a9c79b9ec3937a5642e9c4d04feffad950413acc
Masaki <masaki0320@icloud.com>
2019-02-26 01:30:06 +0200

    player service

---
+++ src/app/shared/services/player.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '@environments/environment.prod';
+import { Observable } from 'rxjs';
+import { Player } from '@core/models/player.model';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerService {
+  _httpOptions: any;
+
+  constructor(
+    private _http: HttpClient,
+    private _router: Router
+  ) {
+    this._httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+  }
+
+  getScores(): Observable<any> {
+    return this._http.get(`${environment.api}/players`);
+  }
+
+  addPlayer(player: Player): Observable<any> {
+    return this._http.post(`${environment.api}/players/add`, player, this._httpOptions).pipe(
+      catchError(e => this._router.navigate['menu'])
+    );
+  }
+
+  removePlayer(player: Player): Observable<any> {
+    return this._http.post(`${environment.api}/players/remove`, player, this._httpOptions).pipe(
+      catchError(e => this._router.navigate['menu'])
+    );
+  }
+}
